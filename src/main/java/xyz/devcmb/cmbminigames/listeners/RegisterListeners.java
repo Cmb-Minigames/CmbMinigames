fabf6a9b3b17c301c9710ee51789d14a05b64be7
DevCmb <71194682+29cmb@users.noreply.github.com>
2024-10-13 13:57:23 -0400

    Rename death listener to death effects and add a lot more death messages

--- src/main/java/xyz/devcmb/cmbminigames/listeners/RegisterListeners.java
+++ src/main/java/xyz/devcmb/cmbminigames/listeners/RegisterListeners.java
@@ -1,9 +1,13 @@
 package xyz.devcmb.cmbminigames.listeners;
 
+import org.bukkit.plugin.PluginManager;
 import xyz.devcmb.cmbminigames.CmbMinigames;
 
 public class RegisterListeners {
     public static void register(){
-        CmbMinigames.getPlugin().getServer().getPluginManager().registerEvents(new DeathListener(), CmbMinigames.getPlugin());
+        CmbMinigames plugin = CmbMinigames.getPlugin();
+        PluginManager pluginManager = plugin.getServer().getPluginManager();
+
+        pluginManager.registerEvents(new DeathEffects(), CmbMinigames.getPlugin());
     }
 }
