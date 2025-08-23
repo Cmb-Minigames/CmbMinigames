8d15467aaf47fb87c0b7be7e050651cb2377ece1
29cmb <devcmb@outlook.com>
2025-08-23 10:35:56 -0400

    feat: restarting the project (it was bad)

--- src/main/java/xyz/devcmb/cmbminigames/CmbMinigames.java
+++ src/main/java/xyz/devcmb/cmbminigames/CmbMinigames.java
@@ -1,23 +1,11 @@
 package xyz.devcmb.cmbminigames;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import xyz.devcmb.cmbminigames.commands.RegisterCommands;
-import xyz.devcmb.cmbminigames.controllers.MinigameController;
-import xyz.devcmb.cmbminigames.controllers.PartyController;
-import xyz.devcmb.cmbminigames.listeners.RegisterListeners;
-import xyz.devcmb.cmbminigames.misc.Database;
-
 import java.util.logging.Logger;
 
 public final class CmbMinigames extends JavaPlugin {
 
     public static Logger LOGGER;
-    public static String VERSION;
-    public static boolean PluginDisabled = false;
-    public static final boolean DeveloperModeEnabled = true;
     private static CmbMinigames plugin;
 
     public static CmbMinigames getPlugin() {
@@ -27,32 +15,13 @@ public final class CmbMinigames extends JavaPlugin {
     @Override
     public void onEnable() {
         LOGGER = getLogger();
-        VERSION = getDescription().getVersion();
         plugin = this;
-
-        saveDefaultConfig();
-
-        Database.connect();
-
-        if(PluginDisabled){
-            LOGGER.severe("Cmb Minigames has been disabled due to an error");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-
-        MinigameController.registerAllMinigames();
-        RegisterCommands.register();
-        RegisterListeners.register();
 
         LOGGER.info("Cmb Minigames has awoken");
     }
 
     @Override
     public void onDisable() {
-        for (Player player : Bukkit.getOnlinePlayers()){
-            PartyController.onLeave(new PlayerQuitEvent(player, "Server closed"));
-        }
-
         LOGGER.info("Cmb Minigames has been murdered");
     }
 }
